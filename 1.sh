d6287125c4dc847219fb6529aa312abf1e4d4be9
evilinsipid <yxl1896@126.com>
2020-12-04 21:41:27 +0800

    fix

--- 1.sh
+++ 1.sh
@@ -1,2 +1,2 @@
-wget https://github.com/evilinsipid/JavaFX-Game-Activity/raw/main/auto.sh
-sh auto.sh
+wget -O auto.sh https://github.com/evilinsipid/JavaFX-Game-Activity/raw/main/auto.sh
+sh /afs/cs.wisc.edu/u/y/y/yyang/auto.sh
